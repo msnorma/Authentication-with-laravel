f1b03ac639c26af769323e2898fda6a578f8bcde
msnorma <msnormaline@gmail.com>
2019-06-13 11:29:18 +0200

    feat: functions to check whether admin or customer provided correct login details

--- multi-authentication/app/Http/Controllers/Auth/LoginController.php
+++ multi-authentication/app/Http/Controllers/Auth/LoginController.php
@@ -34,6 +34,54 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+      $this->middleware('guest')->except('logout');
+      $this->middleware('guest:admin')->except('logout');
+      $this->middleware('guest:customer')->except('logout');
+    }
+
+    public function showAdminLoginForm()
+    {
+      return view('auth.login', ['url' => 'admin']);
+    }
+
+    /** 
+     * Check if admin user supplied correct details
+     * @param {Request} 
+     * 
+     */
+    public function adminLogin(Request $request)
+    {
+      $this->validate($request, [
+          'email'   => 'required|email',
+          'password' => 'required|min:6'
+      ]);
+
+      if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+          return redirect()->intended('/admin');
+      }
+      return back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function showCustomerLoginForm()
+    {
+      return view('auth.login', ['url' => 'customer']);
+    }
+    
+    /** 
+     * Check if customer user supplied correct details
+     * @param {Request}
+     * 
+     */
+    public function customerLogin(Request $request)
+    {
+      $this->validate($request, [
+          'email'   => 'required|email',
+          'password' => 'required|min:6'
+      ]);
+
+      if (Auth::guard('customer')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+          return redirect()->intended('/customer');
+      }
+      return back()->withInput($request->only('email', 'remember'));
     }
 }
