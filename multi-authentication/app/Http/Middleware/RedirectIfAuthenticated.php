0090b3a9c0bece8ac7a3370728c2abcba00011fa
msnorma <msnormaline@gmail.com>
2019-06-13 11:37:48 +0200

    feat: handle how users are redirected to their rightful pages if they authenticated

--- multi-authentication/app/Http/Middleware/RedirectIfAuthenticated.php
+++ multi-authentication/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,16 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        if(Auth::guard($guard)->check()) {
+          return redirect('/home');
+        }
+
+        if($guard == 'admin' && Auth::guard($guard)->check()) {
+          return redirect('/admin');
+        }
+
+        if($guard == 'customer' && Auth::guard($guard)->check()) {
+          return redirect('/customer');
         }
 
         return $next($request);
