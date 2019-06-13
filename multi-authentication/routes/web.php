9e011852d2b6e77e7916b46b828ba22413a74c02
msnorma <msnormaline@gmail.com>
2019-06-13 11:25:06 +0200

    feat: defining web routes for admins and customers

--- multi-authentication/routes/web.php
+++ multi-authentication/routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+/* Routes for Admins and Customer Logins and Registration*/
+Route::get('/login/admin', 'Auth/LoginController@showAdminLoginForm');
+Route::get('/login/customer', 'Auth/LoginController@showCustomerLoginForm');
+Route::get('/register/admin', 'Auth/RegisterController@showAdminRegisterForm');
+Route::get('/register/customer', 'Auth/RegisterController@showCustomerRegisterForm');
+
+/* Routes for Admins and Customers once they have supplied their details */
+Route::get('/login/admin', 'Auth/LoginController@adminLogin');
+Route::get('/login/customer', 'Auth/LoginController@customerLogin');
+
+/* Routes to create new records for Admin and Customer */
+Route::get('/register/admin', 'Auth/RegisterController@createAdmin');
+Route::get('/register/customer', 'Auth/RegisterController@createCustomer');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin');
+Route::view('/customer', 'customer');
+
+
+
